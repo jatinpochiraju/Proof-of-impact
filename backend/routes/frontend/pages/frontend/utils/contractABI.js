fb83a37982a2e0f4ee9f491678a83b88d123734e
nvjatinpochiraju <158489296+jatinpochiraju@users.noreply.github.com>
2025-03-30 22:10:24 +0530

    Create contractABI.js

---
+++ backend/routes/frontend/pages/frontend/utils/contractABI.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        "inputs": [
+            { "internalType": "address", "name": "recipient", "type": "address" },
+            { "internalType": "string", "name": "metadataURI", "type": "string" }
+        ],
+        "name": "mintImpactNFT",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
